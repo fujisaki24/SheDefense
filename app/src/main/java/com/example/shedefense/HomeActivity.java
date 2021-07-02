dbb7ead68e75b8ccf83aa169eaa2a2cba90899a3
fujisaki12 <albycherian303@gmail.com>
2021-07-02 21:06:02 +0530

    working

--- app/src/main/java/com/example/shedefense/HomeActivity.java
+++ app/src/main/java/com/example/shedefense/HomeActivity.java
@@ -1,28 +1,78 @@
 package com.example.shedefense;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class HomeActivity extends AppCompatActivity {
 
     private static final String myPreference = "Camalot";
-
-
-
-    TextView textView;
+    Toolbar toolbar;
+    TextView data_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         SharedPreferences sharedPreferences = getSharedPreferences(myPreference,MODE_PRIVATE);
+        String Database_Name = sharedPreferences.getString("Username","No");
 
-        textView = findViewById(R.id.detail);
+        String full_text = "Good Day,\n" + Database_Name;
 
-        String name = sharedPreferences.getString("Username","");
+        //For toolbar
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        textView.setText(name);
+        data_name = findViewById(R.id.userid_text);
+        data_name.setText(full_text);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        new MaterialAlertDialogBuilder(this,R.style.MyOpaqueAlertDialog)
+                .setTitle("Exit")
+                .setMessage("Are you sure you want to exit?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finishAffinity();
+                        System.exit(0);
+                    }
+                }).setNegativeButton("No",null)
+                .show();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_items,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.settings:
+                Toast.makeText(getApplicationContext(),"Settings not developed",Toast.LENGTH_LONG).show();
+                break;
+            default:
+                Toast.makeText(getApplicationContext(),"Nothing here",Toast.LENGTH_LONG).show();
+        }
+        return true;
     }
 }
